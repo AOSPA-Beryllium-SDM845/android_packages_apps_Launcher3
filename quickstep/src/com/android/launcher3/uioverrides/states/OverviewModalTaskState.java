68ba41d61a3bed15cdb71d979c3f5d4b177e292f
Pranav Vashi <neobuddy89@gmail.com>
2024-03-30 22:50:46 +0530

    Launcher3: Hide meminfo element in OverviewModalTaskState
    Change-Id: Idf33016938fa5d7c0228312c8e2395ead3d43463
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewModalTaskState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewModalTaskState.java
@@ -47,8 +47,16 @@ public class OverviewModalTaskState extends OverviewState {
 
     @Override
     public int getVisibleElements(Launcher launcher) {
+        boolean memInfo = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_meminfo", false);
         boolean clearAll = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_clear_all", true);
-        return OVERVIEW_ACTIONS | (!clearAll ? CLEAR_ALL_BUTTON : 0) | MEMINFO;
+        int elements =  OVERVIEW_ACTIONS;
+	if (!clearAll) {
+            elements |= CLEAR_ALL_BUTTON;
+        }
+	if (memInfo) {
+	    elements |= MEMINFO;
+	}
+	return elements;
     }
 
     @Override
